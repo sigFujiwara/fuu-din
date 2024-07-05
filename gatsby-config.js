73ac2aef2e06924a63f210559db445d1dea8f3b4
k fujiwara <k.fujiwara@sigma-com.co.jp>
2024-07-05 13:33:53 +0900

    Finished Gatsby Tutorial Part 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,16 @@
  */
 module.exports = {
   siteMetadata: {
+    title: "FuuDin-Blog",
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
-}
+  plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
+  ],
+};
